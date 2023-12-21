a4444af41a6745ca23681d6bcb53b6b51b27e7e2
Skynuke <kosmos-omel@yandex.ru>
2023-12-21 11:40:33 +0300

    1st try

---
+++ Rectangular.Tests/Rectangular.Tests.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"..\rectangular\rectangular.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RectangularTests
+{
+	TEST_CLASS(RectangularTests)
+	{
+	public:
+		
+		TEST_METHOD(Point_Valid_Coordinates_Success)
+		{
+			// arrange
+			Point p = Point(2, 3);
+
+			// act
+			double resultX = p.x;
+			double resultY = p.y;
+
+			// assert
+			Assert::AreEqual(2., resultX);
+			Assert::AreEqual(3.,resultY);
+
+		}
+		TEST_METHOD(Rectangular_Constructor_First_IsNotNull) {
+			//arrange
+			auto p = std::make_shared<Point>(2, 3);
+			auto p2 = std::make_shared<Point>(4, 5);
+			Rectangular rec = Rectangular(p,p2);
+			std::stringstream buffer;
+			std::string testString;
+			//act
+			buffer << rec;
+			testString = buffer.str();
+			//assert
+			Assert::IsNotNull(testString.c_str());
+		}
+
+		TEST_METHOD(Point_Output_Operator_Overloading_Success) {
+			//arrange
+			auto p = std::make_shared<Point>(2, 2);
+			std::stringstream buffer;
+			std::string testString;
+			std::string goodString;
+			//act
+			buffer << *p;
+			testString = buffer.str();
+			goodString = "Point(2, 2)";
+			//assert
+
+			Assert::AreEqual(testString, goodString);
+		}
+		
+		
+		TEST_METHOD(Rectangular_Output_Operator_Overloading_Success) {
+			//arrange
+			auto p = std::make_shared<Point>(2, 2);
+			auto p2 = std::make_shared<Point>(3, 4);
+			Rectangular rec = Rectangular(p,p2);
+			std::stringstream buffer;
+			std::string testString;
+			std::string goodString;
+			//act
+			buffer << rec;
+			testString = buffer.str();
+			goodString = "Point(2, 2)\nPoint(3, 2)\nPoint(3, 4)\nPoint(2, 4)\n";
+			//assert
+
+			Assert::AreEqual(testString, goodString);
+		}
+		//ostream point, 2 конструктора (not null, неправильный ввод)
+		
+		
+	};
+}
---
+++ rectangular/rectangular.cpp
@@ -0,0 +1,26 @@
+﻿#include"rectangular.h"
+#include <vector>
+#include<sstream>
+
+
+Rectangular::Rectangular(std::shared_ptr<Point>& leftUp, std::shared_ptr<Point>& rightDown):
+	leftUp(leftUp),rightDown(rightDown)
+{	
+	rectangular_top.push_back(leftUp);
+	
+	auto rightUp= std::make_shared<Point>(rightDown->x, leftUp->y);
+
+	rectangular_top.push_back(rightUp);
+	rectangular_top.push_back(rightDown);
+
+	auto leftDown= std::make_shared<Point>(leftUp->x, rightDown->y );
+	rectangular_top.push_back(leftDown);
+}
+std::ostream& operator<<(std::ostream& out, const Rectangular& rec)
+{
+	std::string f;
+	for (int i = 0; i < rec.rectangular_top.size(); i++) {
+		out <<*rec.rectangular_top[i]<< "\n";
+	}
+	return out;
+};
